bd8b37f7c26498713a3118341c2497679926be1e
fightant1w1ll <nc_8093@163.com>
2022-12-24 15:03:12 +0800

    Fix eslint error

--- src/client/components/SourceEditor/components/SourceView/SourceView.tsx
+++ src/client/components/SourceEditor/components/SourceView/SourceView.tsx
@@ -1,6 +1,8 @@
+import { SourceViewProps } from "./types";
+
 import styles from "./index.css";
 
-export const SourceView = ({ data }) => (
+export const SourceView = ({ data }: SourceViewProps) => (
   <div className={styles.sourceView}>
     <pre>
       <code>{JSON.stringify(data, null, 2)}</code>
